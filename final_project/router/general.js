8521b26bfb071b64f6093b869368960f07a312fd
Muaz Abdeen <muaz.abdeen@cs.huji.ac.il>
2023-06-19 11:51:15 +0300

    ASYNC done ..

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -23,11 +24,30 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', (req, res) => res.send(JSON.stringify(books, null, 4)) );
 
+/* TASK 10 - Get the book list available in the shop asynchronously */
+public_users.get('/books', (req, res) => {
+	const get_books = new Promise( (resolve, reject) => {
+			resolve(res.send(JSON.stringify(books, null, 4)));
+	});
+	get_books.then(() => console.log("Promise for Task 10 resolved"));
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', (req, res) => {
   const ISBN = req.params.isbn;
   books[ISBN] ? res.send(books[ISBN])
               : res.send(`There is no book with ISBN: ${ISBN}`);
+});
+
+/* TASK 11 - Get the book details based on ISBN asynchronously */
+public_users.get('/books/isbn/:isbn', (req, res) => {
+  const ISBN = req.params.isbn;
+  const get_books_isbn = new Promise((resolve, reject) => {
+    books[ISBN] ? resolve(res.send(books[ISBN]))
+                : reject(res.send(`There is no book with ISBN: ${ISBN}`));
+  });
+  get_books_isbn.then( () => console.log("Promise is resolved") )
+                .catch( () => console.log(`There is no book with ISBN: ${ISBN}`) );
 });
   
 // Get book details based on author
@@ -42,6 +62,23 @@ public_users.get('/author/:author', (req, res) => {
   res.send(`No books for ( ${author} )`);
 });
 
+/* TASK 12 - Get the book details based on Author asynchronously */
+public_users.get('/books/author/:author', (req, res) => {
+  const author = req.params.author;
+  const get_books_author = new Promise((resolve, reject) => {
+    let booksByAuthor = [];
+    for (isbn in books) {
+        if (books[isbn]["author"] === author) {
+            booksByAuthor.push({...books[isbn]});
+        }
+    }
+    (booksByAuthor.length > 0) ? resolve(res.send(JSON.stringify({booksByAuthor}, null, 4)))
+                                : reject(res.send(`There is no books for: ${author}`));
+  });
+  get_books_author.then( () => console.log("Promise is resolved") )
+                  .catch( () => console.log(`There is no books for: ${author}`) );
+});
+
 // Get all books based on title
 public_users.get('/title/:title', (req, res) => {
   const title = req.params.title;
@@ -52,6 +89,21 @@ public_users.get('/title/:title', (req, res) => {
     }
   }
   res.send(`No books with the title ( ${title} )`);
+});
+
+/* TASK 13 - Get the book details based on Title asynchronously */
+public_users.get('/books/title/:title', (req, res) => {
+  const title = req.params.title;
+  const get_books_title = new Promise((resolve, reject) => {
+    for (isbn in books) {
+        if (books[isbn]["title"] === title) {
+            resolve(res.send(JSON.stringify(books[isbn], null, 4)))
+        }
+    }
+    reject(res.send(`There is no books with title: ${title}`));
+  });
+  get_books_title.then( () => console.log("Promise is resolved") )
+                  .catch( () => console.log(`There is no books with title: ${title}`) );
 });
 
 //  Get book review
